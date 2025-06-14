d6316019136506c0ab97299246aeab7f3174e6c0
Kavi7605 <110843467+Kavi7605@users.noreply.github.com>
2025-06-14 21:14:44 +0530

    Unit test case for createUser(). SUCCESSFULL!!

--- src/test/java/com/java/firstspringproject/service/UserServiceTest.java
+++ src/test/java/com/java/firstspringproject/service/UserServiceTest.java
@@ -3,21 +3,18 @@ package com.java.firstspringproject.service;
 import com.java.firstspringproject.model.CreateUserRequest;
 import com.java.firstspringproject.model.User;
 import com.java.firstspringproject.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.junit.jupiter.MockitoSettings;
-import org.mockito.quality.Strictness;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.*;
-
-import java.util.Optional;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
-public class UserServiceTest {
+class UserServiceTest {
 
     @Mock
     private UserRepository userRepository;
@@ -28,29 +25,23 @@ public class UserServiceTest {
     @InjectMocks
     private UserService userService;
 
+    private CreateUserRequest createUserRequest;
+
+    @BeforeEach
+    void setUp() {
+        createUserRequest = new CreateUserRequest();
+        createUserRequest.setEmail("test@example.com");
+        createUserRequest.setName("Test User");
+        createUserRequest.setPassword("password123");
+    }
+
     @Test
-//    @MockitoSettings(strictness = Strictness.LENIENT)
-    void createUser_success() {
-        // Arrange
-        CreateUserRequest request = new CreateUserRequest();
-        request.setEmail("test@example.com");
-        request.setName("Test User");
-        request.setPassword("SecurePassword123");
-        request.setPhoneNumber("9876543210");
+    void createUser_ShouldSaveUserToDbAndAuth0() {
+        // When
+        userService.createUser(createUserRequest);
 
-//        when(userRepository.findByEmail("test@example.com")).thenReturn(Optional.empty());
-        when(userRepository.findByEmail(anyString())).thenReturn(Optional.empty());
-        when(userRepository.findByEmail(anyString())).thenReturn(Optional.empty());
-
-        // Act
-        userService.createUser(request);
-
-        // Assert
-        verify(auth0Service, times(1)).createUserInAuth0(
-                eq("test@example.com"),
-                eq("SecurePassword123")
-        );
-
-        verify(userRepository, times(1)).save(any(User.class));
+        // Then
+        verify(userRepository).save(any(User.class));
+        verify(auth0Service).createUserInAuth0(createUserRequest.getEmail(), createUserRequest.getPassword());
     }
 }
